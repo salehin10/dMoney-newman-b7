047afc77edcf5f7c9210fe5783d603b5240db9b7
Siraj <ssalehin@relisource.com>
2023-05-25 03:14:01 +0600

    05/25/23/ 3:12 AM- Pushing all files to server

---
+++ report.js
@@ -0,0 +1,18 @@
+const newman = require('newman');
+
+newman.run({
+    collection: require("./collection/dMoney.json"),
+    //collection :
+    //environment: require('./collection/dMoney.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html',
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+   
+})
